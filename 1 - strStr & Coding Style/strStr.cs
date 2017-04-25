ee1598ae9c5d21965d24fff48d116ccc9b4f55c9
LiLi <kflili@gmail.com>
2017-04-25 17:53:59 -0400

    init

---
+++ 1 - strStr & Coding Style/strStr.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace strStr
+{
+    public class solution
+    {
+        https://github.com/kflili/LintCode-C--solution.git
+        /*
+        For a given source string and a target string, you should output the first 
+        index(from 0) of target string in source string. If target does not exist 
+        in source, just return -1.
+        Example:
+        If source = "source" and target = "target", return -1.
+        If source = "abcdabcdefg" and target = "bcd", return 1.
+        */
+        public int strStr(string source, string target)
+        {
+            if (source == null || target == null)
+            {
+                return -1;
+            }
+            for (int i = 0; i < source.Length - target.Length + 1; i++)
+            {
+                int j = 0;
+                for (j = 0; j < target.Length; j++)
+                {
+                    if (source[i + j] != target[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == target.Length)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ code/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace code
+{
+    public class Solution
+    {
+        /**
+         * @param nums a mountain sequence which increase firstly and then decrease
+         * @return then mountain top
+         */
+        public int mountainSequence(int[] nums)
+        {
+            int left = 0, right = nums.Length;
+            while (left + 1 < right)
+            {
+                int m1 = left + (right - left) / 2;
+                int m2 = right - (right - m1) / 2;
+                if (nums[m1] < nums[m2])
+                {
+                    left = m1;
+                } else if (nums[m1] > nums[2])
+                {
+                    right = m2;
+                } else
+                {
+                    left = m1;
+                    right = m2;
+                }
+            }
+            return nums[left] > nums[right] ? nums[left] : nums[right];
+        }
+    }
+}
