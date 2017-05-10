01d5f240d481548fe2c9ff54643614d23ed55dd0
LiLi <kflili@gmail.com>
2017-05-10 14:20:50 -0400

    add code

---
+++ 5 - Depth First Search/Permutations.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace subsetsWithDup
+{
+    public class Solution
+    {
+        public List<List<int>> permuteUnique(int[] nums) {
+            List<List<int>> results = new List<List<int>>();
+            if (nums == null) {
+                return results;
+            }
+            List<int> list = new List<int>();
+            if (nums.Length == 0) {
+                results.Add(list);
+                return results;
+            }
+            Array.Sort(nums);
+            int[] visited = new int[nums.Length];
+            for (int i = 0; i < visited.Length; i++) {
+                visited[i] = 0;
+            }
+                helper(nums, visited, list, results);
+            return results;
+        }
+        private void helper(int[] nums, int[] visited, List<int> list, List<List<int>> results) {
+            if (list.Count == nums.Length) {
+                results.Add(new List<int>(list));
+                return;
+            }
+            for (int i = 0; i < nums.Length; i++) {
+                if (visited[i] == 1) {
+                    continue;
+                }
+                list.Add(nums[i]);
+                visited[i] = 1;
+                helper(nums, visited, list, results);
+                visited[i] = 0;
+                list.RemoveAt(list.Count - 1);
+            }
+        }
+    }
+}
